e7966461374cbdceb83494168979dd197ab151f6
Ciara Gauntt <ci.gauntt@gmail.com>
2022-09-21 17:44:04 -0500

    added questions.js

---
+++ Javascript/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "alerts", "booleans", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within _____. ",
+        choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in javascript can be used to store...",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool being used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+]
